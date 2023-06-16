08c795783e7de5b58efe540dfe636833ed371a5d
Ayano <s.correa.wilches@gmail.com>
2023-06-15 19:16:41 -0500

    add project configuration

---
+++ app/ApolloWrapper.tsx
@@ -0,0 +1,68 @@
+"use client";
+// ^ this file needs the "use client" pragma
+
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  SuspenseCache,
+} from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+/*
+THIS IS FOR SSR
+*/
+
+// have a function to create a client for you
+function makeClient() {
+  const httpLink = new HttpLink({
+    // this needs to be an absolute url, as relative urls cannot be used in SSR
+    uri: "https://ace-swift-28.hasura.app/v1/graphql",
+    headers: {
+      "x-hasura-admin-secret":
+        "LYrCQsiD5ZjKOyX1wzD3VFVzcqbidOm9r4PShsJYxBUV46QSnMMgupQPWLvaNOnv",
+      "Access-Control-Allow-Origin": "*", // CORS
+    },
+    // you can disable result caching here if you want to
+    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+    fetchOptions: { cache: "no-store" },
+  });
+
+  return new ApolloClient({
+    // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            // in a SSR environment, if you use multipart features like
+            // @defer, you need to decide how to handle these.
+            // This strips all interfaces with a `@defer` directive from your queries.
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+// also have a function to create a suspense cache
+function makeSuspenseCache() {
+  return new SuspenseCache();
+}
+
+// you need to create a component to wrap your app in
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider
+      makeClient={makeClient}
+      makeSuspenseCache={makeSuspenseCache}
+    >
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
